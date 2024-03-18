8a6fa85fd2c149eb9f46315136d78616b32668e0
mahya <beheshtimahya11@gmail.com>
2024-03-18 22:21:56 +0330

    nft is mintable

---
+++ Solidity/contracts/NFTMaker.sol
@@ -0,0 +1,37 @@
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract NFT is ERC721, ERC721URIStorage, Ownable {
+    constructor(
+        address initialOwner,
+        string memory nftName,
+        string memory nftSymbol
+    ) ERC721(nftName, nftSymbol) Ownable(initialOwner) {}
+
+    function safeMint(
+        address to,
+        uint256 tokenId,
+        string memory uri
+    ) public onlyOwner {
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721, ERC721URIStorage) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+}
